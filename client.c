a3e53caf46ca576642206fba71671c2f23504910
Mohammed ali Kellati <mokellat@e1r5p11.1337.ma>
2021-06-17 21:50:31 +0100

    done to be remake tomorrow

--- client.c
+++ client.c
@@ -6,13 +6,13 @@
 /*   By: mokellat <mokellat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 09:07:42 by mokellat          #+#    #+#             */
-/*   Updated: 2021/06/17 20:03:17 by mokellat         ###   ########.fr       */
+/*   Updated: 2021/06/17 21:35:07 by mokellat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	str_to_bin(char *str)
+int	*str_to_bin(char *str)
 {
 	int	i;
 	int	lenght;
@@ -48,15 +48,28 @@ void	str_to_bin(char *str)
 		if((i + 1) % 8 == 0 && i != 0)
 			printf(" ");
 	}
+	return (binary);
 }
 
 int	main(int argc, char **argv)
 {
+	int *binary;
+	int	i;
+
+	i = -1;
 	if(argc != 3)
 	{
 		ft_putstr("Arguments are invalid");
 		printf("1");
 		exit(EXIT_FAILURE);
 	}
-	str_to_bin(argv[2]);
+	binary = str_to_bin(argv[2]);
+	while(++i < (8 * (int)ft_strlen(argv[2])))
+	{
+		if (binary[i] == 1)
+			kill(ft_atoi(argv[1]), SIGUSR1);
+		else
+			kill(ft_atoi(argv[1]), SIGUSR2);
+		usleep(100);
+	}
 }
--- server.c
+++ server.c
@@ -6,19 +6,31 @@
 /*   By: mokellat <mokellat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 09:19:02 by mokellat          #+#    #+#             */
-/*   Updated: 2021/06/16 11:48:50 by mokellat         ###   ########.fr       */
+/*   Updated: 2021/06/17 21:49:53 by mokellat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
+char n;
+int i;
 void	func(int nb)
 {
-	printf("%d \n", nb);
+	if (nb == SIGUSR1)
+		n += 1 << i;
+	i--;
+	if (i == -1)
+	{
+		// printf("\n%d\n", n);
+		write(1, &n, 1);
+		n = 0;
+		i = 7;
+	}
 }
 
 int	main(void)
 {
+	i =7;
+	n = 0;
 	signal(SIGUSR1, func);
 	signal(SIGUSR2, func);
 	ft_putstr("the PID is : ");
